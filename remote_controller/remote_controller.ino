69ed97836b3ee8f8ef3de8a804d4226620b94cee
Yifan <guyifan1121@gmail.com>
2025-01-21 14:59:08 -0800

    remote_controller: Combined UWB_tag with the remote controller.

--- remote_controller/remote_controller.ino
+++ remote_controller/remote_controller.ino
@@ -1,4 +1,8 @@
 #include <ArduinoBLE.h>
+#include <SPI.h>
+
+#include "DW1000Ranging.h"
+#include "DW1000.h"
 
 #define DATA_RATE 100 // 100 Hz
 
@@ -43,6 +47,24 @@ unsigned int lastPingTime = 0;
 unsigned long lastScanMillis = 0;
 unsigned long scanIntervalMillis = 1000;
 
+
+// DW1000 constants.
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+
+// connection pins
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 4;   // spi select pin
+
+// TAG antenna delay defaults to 16384
+uint16_t Adelay = 16384;
+
+// leftmost two bytes below will become the "short address"
+char tag_addr[] = "7D:00:22:EA:82:60:3B:9C";
+
 void setup() {
   // Start Serial for debugging
   unsigned long startTime = millis();
@@ -53,15 +75,33 @@ void setup() {
     // Wait for Serial or timeout
   }
 
+  setupUWBTag();
   setupBLEPeripheral();
   connectToCentral();
 }
 
 void loop() {
   readStatusData();
+  DW1000Ranging.loop();
   readControllerData();
   sendControllerData(); // TODO(yifan): Send update only when there's a value change.
   delay(1000 / DATA_RATE); // Control the data rate.
+}
+
+void setupUWBTag() {
+  //init the configuration
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  // set antenna delay for anchors only. Tag is default (16384)
+  //DW1000.setAntennaDelay(Adelay);
+
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+  //start the module as an anchor, do not assign random short address
+  DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
 }
 
 void setupBLEPeripheral() {
@@ -146,6 +186,18 @@ void readStatusData() {
     //Serial.printf("Data interval: %d(ms)\n", millis() - lastPingTime);
     //lastPingTime = millis();
   }
+}
+
+void newRange() {
+  Serial.printf("Anchor address=%X, distance=%d(m)\n", DW1000Ranging.getDistantDevice()->getShortAddress(), DW1000Ranging.getDistantDevice()->getRange());
+}
+
+void newDevice(DW1000Device *device) {
+  Serial.printf("Anchor connected, address=%X\n", device->getShortAddress());
+}
+
+void inactiveDevice(DW1000Device *device) {
+  Serial.printf("Anchor disconnected, address=%X\n", device->getShortAddress());
 }
 
  // TODO(yifan): Fill me in.
